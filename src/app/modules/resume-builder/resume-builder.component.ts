7054401aaf8eff54d10ce73039680b0641c8f699
guglextech <guglex.technologies@gmail.com>
2025-10-27 15:42:47 +0000

    [WIP] +> change layout

--- src/app/modules/resume-builder/resume-builder.component.ts
+++ src/app/modules/resume-builder/resume-builder.component.ts
@@ -17,6 +17,9 @@ export class ResumeBuilderComponent implements OnInit {
   isMobileSidebarOpen = false;
   isMobile = false;
   currentPageTitle = 'Resumes';
+  showHeaderActions = false;
+  showTemplates = false;
+  currentComponent: any;
 
   constructor(
     private router: Router,
@@ -50,24 +53,40 @@ export class ResumeBuilderComponent implements OnInit {
       });
   }
 
-  updatePageTitle(url: string) {
+  updatePageTitle(url: any) {
     if (url.includes('getting-started')) {
       this.currentPageTitle = 'Get Started';
+      this.showHeaderActions = false;
     } else if (url.includes('settings')) {
       this.currentPageTitle = 'Settings';
+      this.showHeaderActions = false;
     } else if (url.includes('ats-analysis')) {
       this.currentPageTitle = 'ATS Analysis';
+      this.showHeaderActions = false;
     } else if (url.includes('cv-rewrite')) {
       this.currentPageTitle = 'CV Rewrite';
-    } else if (url.includes('resume-builder')) {
+      this.showHeaderActions = false;
+    } else if (url.includes('resume-builder') || url.includes('cv-sections')) {
       this.currentPageTitle = 'Resume Builder';
+      this.showHeaderActions = true;
     } else {
       this.currentPageTitle = 'Resumes';
+      this.showHeaderActions = false;
     }
   }
 
   onRouteActivate(component: any) {
-    // Handle component activation
+    this.currentComponent = component;
+    if (component.showTemplates !== undefined) {
+      this.showTemplates = component.showTemplates;
+    }
+  }
+
+  toggleTemplates() {
+    if (this.currentComponent && this.currentComponent.toggleTemplates) {
+      this.currentComponent.toggleTemplates();
+      this.showTemplates = this.currentComponent.showTemplates;
+    }
   }
 
   toggleSidebar() {
